3c9cc70b96250b5d8f564eb801fa3dd5a3863a50
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-09-04 05:03:26 +0600

    account controller working

--- ProductCRUD.CLIENT/Controllers/AccountController.cs
+++ ProductCRUD.CLIENT/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ProductCRUD.CLIENT.DTOs;
 using ProductCRUD.CLIENT.Models;
 
 namespace ProductCRUD.CLIENT.Controllers;
@@ -7,11 +6,12 @@ namespace ProductCRUD.CLIENT.Controllers;
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
-    HttpClient _client = new HttpClient();
+    private readonly HttpClient _client;
 
     public AccountController(ILogger<AccountController> logger)
     {
         _logger = logger;
+        _client = new HttpClient();
     }
 
 
@@ -20,23 +20,36 @@ public class AccountController : Controller
     {
         return View();
     }
-     
+      
     [HttpPost]
     public async Task<IActionResult> Login(UserModel user)
     {
         try
         {
-            _client.BaseAddress = new Uri("https://stg-zero.propertyproplus.com.au/api/TokenAuth");
-            HttpResponseMessage response = await _client.PostAsJsonAsync("/Authenticate", user);
-            if (response.IsSuccessStatusCode)
-            {
-                return RedirectToAction("Index");
-            }
+            // Set the base address for the API
+            _client.BaseAddress = new Uri("https://stg-zero.propertyproplus.com.au");
+
+            // Set the tenant ID in the header
+            _client.DefaultRequestHeaders.Add("Abp.TenantId", "10");
+
+            // Serialize the user object to JSON and send it as the request body
+            HttpResponseMessage response = await _client.PostAsJsonAsync("/api/TokenAuth/Authenticate", user);
+
+            // Check if the response is successful (status code 200)
+            if (!response.IsSuccessStatusCode) return View(response);
+            
+            // Read the response content as a string
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            // Set the response content in ViewBag
+            ViewBag.ResponseContent = "hi" + responseContent;
+
             return View(user);
+
         }
         catch (Exception ex)
         {
-            _logger.LogError("HomeController > AddProduct Error: " + ex.ToString());
+            _logger.LogError("AccountController > Login Error: " + ex.ToString());
             return View(null);
         }
     }

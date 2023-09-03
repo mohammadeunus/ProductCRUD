c31f11c7f46385e7743056a91b49f08bda11fff2
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-09-04 04:27:29 +0600

    account login controller added

---
+++ ProductCRUD.CLIENT/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCRUD.CLIENT.DTOs;
+using ProductCRUD.CLIENT.Models;
+
+namespace ProductCRUD.CLIENT.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    HttpClient _client = new HttpClient();
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+     
+    [HttpPost]
+    public async Task<IActionResult> Login(UserModel user)
+    {
+        try
+        {
+            _client.BaseAddress = new Uri("https://stg-zero.propertyproplus.com.au/api/TokenAuth");
+            HttpResponseMessage response = await _client.PostAsJsonAsync("/Authenticate", user);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("HomeController > AddProduct Error: " + ex.ToString());
+            return View(null);
+        }
+    }
+}
---
+++ ProductCRUD.CLIENT/Models/UserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductCRUD.CLIENT.Models;
+
+public class UserModel
+{
+    public string userNameOrEmailAddress { get; set; }
+    public string password { get; set; }
+}
